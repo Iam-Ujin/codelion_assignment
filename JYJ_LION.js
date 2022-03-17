adb50fa8fb88c5adc868c72c6e645bf9216eafa3
ujin <dklvmf0106@gmail.com>
2022-03-17 16:05:25 +0900

    feat : 회원가입, 아이디/비밀번호찾기 추가

--- JYJ_LION.js
+++ JYJ_LION.js
@@ -1,7 +1,9 @@
+// 로그인 페이지 닫기버튼
 $(".closeBtn").on("click", function () {
   $(".container").slideUp();
 });
 
+// 텍스트 hover시 글자 색상 변경
 $(".hover").on("mouseover", function () {
   $(this).addClass("hoverEvent");
 });
@@ -9,6 +11,7 @@ $(".hover").on("mouseleave", function () {
   $(this).removeClass("hoverEvent");
 });
 
+// 로그인 상태 유지 체크 토글
 var count = 0;
 $(".loginCheck").on("click", function () {
   count += 1;
@@ -23,4 +26,48 @@ $(".loginCheck").on("click", function () {
     });
     $(".checkIcon").attr("src", "./JYJ_LION_img/icon_check_empty.png");
   }
+});
+
+// 회원가입 버튼 클릭
+$(".join").on("click", function () {
+  $(".loginBox").slideUp();
+  $(".joinBox").slideDown();
+});
+
+// 회원가입 닫기 버튼
+$(".joinCloseBtn").on("click", function () {
+  $(".loginBox").slideDown();
+  $(".joinBox").slideUp();
+});
+
+// 회원가입 완료 버튼 클릭
+$(".joinBtn").on("click", function (e) {
+  if (
+    $(".name_val").val() == "" ||
+    $(".id_val").val() == "" ||
+    $(".pw1_val").val() == "" ||
+    $(".pw2_val").val() == ""
+  ) {
+    alert("빈칸을 채워주세요 🥺");
+  } else {
+    alert("회원가입 완료 😁");
+    $(".name_val").val("");
+    $(".id_val").val("");
+    $(".pw1_val").val("");
+    $(".pw2_val").val("");
+    $(".loginBox").slideDown();
+    $(".joinBox").slideUp();
+  }
+});
+
+// 아이디찾기 버튼 클릭
+$(".findId").on("click", function () {
+  $(".loginBox").slideUp();
+  $(".findIdBox").slideDown();
+});
+
+// 아이디찾기 닫기 버튼
+$(".findIdCloseBtn").on("click", function () {
+  $(".loginBox").slideDown();
+  $(".findIdBox").slideUp();
 });
